f4b22984b1a75e0d74fe0e920b4a75b1c029f257
Rory O'Day <rory.oday@wsu.edu>
2022-11-03 15:13:48 -0700

    Add tests

--- src/test/java/org/jabref/logic/exporter/CsvExportFormatTest.java
+++ src/test/java/org/jabref/logic/exporter/CsvExportFormatTest.java
@@ -1,12 +1,17 @@
 package org.jabref.logic.exporter;
 
 import java.io.File;
+import java.io.Reader;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.jabref.logic.layout.LayoutFormatterPreferences;
 import org.jabref.logic.xmp.XmpPreferences;
 import org.jabref.model.database.BibDatabaseContext;
@@ -114,5 +119,44 @@ public class CsvExportFormatTest {
         assertEquals(
                 "10,\"\",\"\",\"\",\"\",\"\",,,\"\",\"\",,\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"von Neumann, John; Smith, John; Black Brown, Peter\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\"",
                 lines.get(1));
+    }
+    @Test
+    public void testFieldWithDelimiters(@TempDir Path testFolder) throws Exception {
+        Path path = testFolder.resolve("ThisIsARandomlyNamedFile");
+
+        BibEntry entry = new BibEntry();
+
+        entry.setField(StandardField.TITLE, "\"test1,\",\"test2,\"");
+
+        List<BibEntry> entries = Arrays.asList(entry);
+
+        exportFormat.export(databaseContext, path, entries);
+        //make sure single quotes replace with double quotes
+        List<String> lines = Files.readAllLines(path);
+        assertEquals(2, lines.size());
+        assertEquals(
+                "10,\"\",\"\",\"\",\"\"\"test1,\"\",\"\"test2,\"\"\",\"\",,,\"\",\"\",,\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\"",
+                lines.get(1));
+    }
+
+    @Test
+    public void testCSVFileWithFieldDelimiters(@TempDir Path testFolder) throws Exception {
+        Path path = testFolder.resolve("ThisIsARandomlyNamedFile");
+
+        String delimitedName = "\"test1,\",\"test2,\"";
+        BibEntry entry = new BibEntry();
+        entry.setField(StandardField.TITLE, delimitedName);
+
+        List<BibEntry> entries = Arrays.asList(entry);
+        exportFormat.export(databaseContext, path, entries);
+        List<String> lines = Files.readAllLines(path);
+        Reader in = new StringReader(lines.get(1));
+        // read data as csv file
+        CSVParser parser = new CSVParser(in, CSVFormat.DEFAULT);
+        CSVRecord list = parser.getRecords().get(0);
+        // number of columns = 31
+        assertEquals(31, list.size());
+        //make sure author does NOT split to two different columns
+        assertEquals(delimitedName, list.get(4));
     }
 }
